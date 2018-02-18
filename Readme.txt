4788c5477559ba7164283585c838f9e155bfd23c
Ariel Levy <ariel.levi@motorolasolutions.com>
2018-02-18 15:54:54 +0200

    add change 4

--- Readme.txt
+++ Readme.txt
@@ -2,3 +2,4 @@ This is only a test
 change 1
 change 2
 change 3
+adding change number 10
--- test1.c
+++ test1.c
@@ -1,6 +1,7 @@
 int main()
 {
   printf("testing GIT version 1\n");
+  printf("add change number 10\n");
   printf("end of test\n");
   return 0;	
 }
